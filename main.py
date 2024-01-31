6503534a4ef1c0d054051dfca4e8f97a36f716f0
Maria Luísa <mlss@cin.ufpe.br>
2024-01-31 18:02:49 -0300

    delete classes instances

--- main.py
+++ main.py
@@ -2,26 +2,16 @@ from genetic_algorithm import GA
 from evolutionary_strategy import ES
 
 def main():
-    genetic_ackley = GA("ackley")
-    genetic_rastrigin = GA("rastrigin")
-    genetic_schwefel = GA("schwefel")
-    genetic_rosenbrock = GA("rosenbrock")
+    # GA("ackley").execute()
+    # GA("rastrigin").execute()
+    # GA("schwefel").execute()
+    # GA("rosenbrock").execute()
 
-    # genetic_ackley.execute()
-    # genetic_rastrigin.execute()
-    # genetic_rosenbrock.execute()
+    # ES("ackley", max_iterations=10, number_of_crossovers=350).execute()
+    # ES("rastrigin", max_iterations=20, number_of_crossovers=350).execute()
+    ES("schwefel", max_iterations=100, number_of_crossovers=350).execute()
+    # ES("rosenbrock", max_iterations=50, number_of_crossovers=350, mutation_prob=0.05).execute()
 
-    # es_ackley = ES("ackley", max_iterations=10, number_of_crossovers=350)
-    # es_ackley.execute()
-
-    # es_rastrigin = ES("rastrigin", max_iterations=20, number_of_crossovers=350)
-    # es_rastrigin.execute()
-
-    es_schwefel = ES("schwefel", max_iterations=100, number_of_crossovers=350)
-    es_schwefel.execute()
-
-    # es_rosenbrock = ES("rosenbrock", max_iterations=50, number_of_crossovers=350, mutation_prob=0.05)
-    # es_rosenbrock.execute()
 
 if __name__ == "__main__":
     main()
